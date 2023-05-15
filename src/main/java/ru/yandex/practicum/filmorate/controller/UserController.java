cd785ed8f4e564d75ba2537587d0aff5fb5eb6d0
nicola <nicolai.vakhlachev@novsu.ru>
2023-05-16 00:11:06 +0300

    fix: в UserController добавлен custom exception

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -1,9 +1,7 @@
 package ru.yandex.practicum.filmorate.controller;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 import ru.yandex.practicum.filmorate.model.User;
 
 import javax.validation.Valid;
@@ -33,9 +31,7 @@ public class UserController {
     @PutMapping
     public User update(@Valid @RequestBody User user) {
         if (user.getId() > users.size()) {
-            throw new ResponseStatusException(
-                    HttpStatus.NOT_FOUND, "the user is not found"
-            );
+            throw new DataNotFoundException("the user is not found");
         }
 
         validate(user);
